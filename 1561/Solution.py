5f088e1145ebecfb2817298ef90c8bae2da227c3
samjwu <sjwu@ualberta.ca>
2023-11-23 19:32:50 -0700

    1561

---
+++ 1561/Solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxCoins(self, piles: List[int]) -> int:
+        n = len(piles)
+        
+        piles.sort()
+        
+        ans = 0
+        
+        for i in range(n//3, n, 2):
+            ans += piles[i]
+            
+        return ans
