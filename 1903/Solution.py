5fded4485fa665cd7508dbca98055043c33e68d3
samjwu <sjwu@ualberta.ca>
2023-12-06 18:59:44 -0700

    1903

---
+++ 1903/Solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def largestOddNumber(self, num: str) -> str:
+        last = len(num)-1
+        
+        while last >= 0 and int(num[last]) % 2 == 0:
+            last -= 1
+            
+        return num[0:last+1]
