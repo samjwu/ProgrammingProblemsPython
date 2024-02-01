6c5975e3835018962d268fc5f85bd3f271f5004e
samjwu <sjwu@ualberta.ca>
2024-01-31 17:43:26 -0700

    2966

---
+++ 2966/Solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def divideArray(self, nums: List[int], k: int) -> List[List[int]]:
+        nums.sort()
+        
+        n = len(nums)
+        
+        ans = []
+        
+        for i in range(0, n, 3):
+            if nums[i+2] - nums[i] > k:
+                return []
+            
+            ans.append([nums[i], nums[i+1], nums[i+2]])
+            
+        return ans
