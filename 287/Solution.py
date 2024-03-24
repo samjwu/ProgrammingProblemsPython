f03741e1364834be3f92995a4b6d16fbdbc85229
samjwu <sjwu@ualberta.ca>
2024-03-23 18:14:10 -0600

    287

---
+++ 287/Solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        # convert to linked list
+        # reason: since there is a duplicate, there is a loop
+        slow = nums[0]
+        fast = nums[0]
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            
+            if slow == fast:
+                break
+        
+        # floyd cycle detection algorithm to get start of loop
+        slow = nums[0];
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+            
+        return slow
