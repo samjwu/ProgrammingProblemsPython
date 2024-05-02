ade688253510b9ca043ac145e0ef03ed19bc70a5
samjwu <22262939+samjwu@users.noreply.github.com>
2024-05-01 20:45:02 -0600

    2441

---
+++ 2441/Solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findMaxK(self, nums: List[int]) -> int:
+        ans = -1
+        
+        seen = set()
+        
+        for num in nums:
+            if -num in seen:
+                ans = max(ans, abs(num))
+                
+            seen.add(num)
+            
+        return ans
