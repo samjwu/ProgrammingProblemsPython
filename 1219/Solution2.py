617da0323e7ec450bec9f13136d43ee996c66c9a
samjwu <22262939+samjwu@users.noreply.github.com>
2024-05-13 18:26:31 -0600

    1219 2

---
+++ 1219/Solution2.py
@@ -0,0 +1,39 @@
+class Solution:
+    def getMaximumGold(self, grid: List[List[int]]) -> int:
+        MOVES = [
+            (-1, 0),
+            (0, -1),
+            (0, 1),
+            (1, 0),
+        ]
+
+        m = len(grid)
+        n = len(grid[0])
+
+        ans = 0
+
+        def dfs(grid: List[List[int]], m: int, n: int, i: int, j: int) -> int:
+            if i < 0 or j < 0 or i == m or j == n or grid[i][j] == 0:
+                return 0
+
+            ans = 0
+
+            # save value
+            gold = grid[i][j]
+
+            # mark visited
+            grid[i][j] = 0
+
+            for dx, dy in MOVES:
+                        ans = max(ans, dfs(grid, m, n, i + dx, j + dy))
+
+            # backtrack
+            grid[i][j] = gold
+
+            return ans + gold
+
+        for i in range(m):
+            for j in range(n):
+                ans = max(ans, dfs(grid, m, n, i, j))
+
+        return ans
