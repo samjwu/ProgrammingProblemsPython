a0c9bd79b2884f0811dc0c9378f74699a6db0ec3
samjwu <22262939+samjwu@users.noreply.github.com>
2024-05-28 18:34:25 -0600

    1404 2

---
+++ 1404/Solution2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def numSteps(self, s: str) -> int:
+        def addOne(st: List[str]) -> None:
+            n = len(st)
+            
+            for i in range(n-1, -1, -1):
+                if st[i] == "1":
+                    st[i] = "0"
+                else:
+                    st[i] = "1"
+                    return
+            
+            st.insert(0, "1")
+            
+        ans = 0
+        st = [c for c in s]
+        
+        while len(st) > 1:
+            ans += 1
+            
+            if st[-1] == "0":
+                st.pop()
+            else:
+                addOne(st)
+        
+        return ans
