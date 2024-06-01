ebb03e645b1c4d383050f3e580013822c255ffcf
samjwu <22262939+samjwu@users.noreply.github.com>
2024-05-31 18:34:54 -0600

    3110

---
+++ 3110/Solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        n = len(s)
+        
+        ans = 0
+        
+        for i in range(1, n):
+            ans += abs(ord(s[i]) - ord(s[i-1]))
+            
+        return ans
