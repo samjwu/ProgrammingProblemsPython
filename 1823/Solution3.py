101ec39187943d8c96baea628d2e5c9a4328bdf1
samjwu <22262939+samjwu@users.noreply.github.com>
2024-07-07 19:04:53 -0600

    1823 3

--- 1823/Solution3.py
+++ 1823/Solution3.py
@@ -5,9 +5,10 @@
 # f(n, k) = (f(n-1, k) - k) % n
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        ans = 0
+        return self.recurse(n, k) + 1
+    
+    def recurse(self, n: int, k: int) -> int:
+        if n == 1:
+            return 0
         
-        for i in range(2, n+1):
-            ans = (ans + k) % i
-        
-        return ans + 1
+        return (self.recurse(n-1, k) + k) % n
