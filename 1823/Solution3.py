8b83270918adabb5936d76680d611e89e67e1ec6
samjwu <22262939+samjwu@users.noreply.github.com>
2024-07-07 18:59:09 -0600

    1823 3

---
+++ 1823/Solution3.py
@@ -0,0 +1,12 @@
+# removing kth person and setting new 0th index
+# is equivalent to shifting circle by -k
+# f(n, k) = (f(n-1, k) - k) % n
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        return self.recurse(n, k) + 1
+    
+    def recurse(self, n: int, k: int) -> int:
+        if n == 1:
+            return 0
+        
+        return (self.recurse(n-1, k) + k) % n
