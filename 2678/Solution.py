db2bb143698f10535b050ef05d83ae3b399a8d9a
samjwu <22262939+samjwu@users.noreply.github.com>
2024-07-31 18:49:29 -0600

    2678

---
+++ 2678/Solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        ans = 0
+        
+        for detail in details:
+            age = int(detail[11])*10 + int(detail[12])
+            if age > 60:
+                ans += 1
+        
+        return ans
