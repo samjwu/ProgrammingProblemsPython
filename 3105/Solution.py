fa04dbbbdb0a45721f7a35d735b35e2668eec4f0
Sam Wu <22262939+samjwu@users.noreply.github.com>
2025-02-02 20:06:22 -0700

    3105

---
+++ 3105/Solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def longestMonotonicSubarray(self, nums: List[int]) -> int:
+        n = len(nums)
+        ans = 1
+
+        incr = 1
+        decr = 1
+
+        for i in range(n-1):
+            if nums[i] < nums[i+1]:
+                incr += 1
+                decr = 1
+            elif nums[i] > nums[i+1]:
+                incr = 1
+                decr += 1
+            else:
+                incr = 1
+                decr = 1
+
+            ans = max(ans, incr, decr)
+
+        return ans
