0dbbca86756a2b7d08fea614922a0a20b38bb0c8
Sam Wu <22262939+samjwu@users.noreply.github.com>
2025-03-18 19:07:44 -0600

    3191 3

---
+++ 3191/Solution3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minOperations(self, nums):
+        n = len(nums)
+        ans = 0
+
+        for i in range(n - 2):
+            if nums[i] == 0:
+                nums[i] = 1
+                nums[i + 1] ^= 1
+                nums[i + 2] ^= 1
+                ans += 1
+
+        if nums[n - 2] == 0 or nums[n - 1] == 0:
+            return -1
+
+        return ans
