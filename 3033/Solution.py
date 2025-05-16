44a2c4ee5e0690f8acea04eac42223bb059f0afa
Sam Wu <22262939+samjwu@users.noreply.github.com>
2025-05-15 20:36:48 -0600

    rename cols to cols_max

--- 3033/Solution.py
+++ 3033/Solution.py
@@ -3,15 +3,15 @@ class Solution:
         m = len(matrix)
         n = len(matrix[0])
 
-        cols = [-1] * n
+        cols_max = [-1] * n
 
         for i in range(m):
             for j in range(n):
-                cols[j] = max(cols[j], matrix[i][j])
+                cols_max[j] = max(cols_max[j], matrix[i][j])
 
         for i in range(m):
             for j in range(n):
                 if matrix[i][j] == -1:
-                    matrix[i][j] = cols[j]
+                    matrix[i][j] = cols_max[j]
 
         return matrix
