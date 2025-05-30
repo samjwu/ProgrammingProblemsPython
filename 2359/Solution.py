e18f741eb46f35fbf9480dd306e1ba885d8fa62d
Sam Wu <22262939+samjwu@users.noreply.github.com>
2025-05-29 18:44:00 -0600

    Rename dfs to compute_dists

--- 2359/Solution.py
+++ 2359/Solution.py
@@ -7,8 +7,8 @@ class Solution:
         dists1 = [-1] * n
         dists2 = [-1] * n
 
-        self.dfs(node1, 0, edges, dists1)
-        self.dfs(node2, 0, edges, dists2)
+        self.compute_dists(node1, 0, edges, dists1)
+        self.compute_dists(node2, 0, edges, dists2)
 
         for i in range(n):
             # check reachable nodes with minimal distance
@@ -18,7 +18,7 @@ class Solution:
 
         return ans
 
-    def dfs(self, node: int, curr_dist: int, edges: List[int], dists: List[int]) -> None:
+    def compute_dists(self, node: int, curr_dist: int, edges: List[int], dists: List[int]) -> None:
         # traverse non-terminal and unvisited nodes
         while node != -1 and dists[node] == -1:
             dists[node] = curr_dist
