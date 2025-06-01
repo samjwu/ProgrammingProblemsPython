4472db962fe4be05d2c0d012effae942f3d073af
Sam Wu <22262939+samjwu@users.noreply.github.com>
2025-05-31 19:01:11 -0600

    2929

---
+++ 2929/Solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def distributeCandies(self, n: int, limit: int) -> int:
+        ans = 0
+
+        # try all possible distributions for 1st child
+        for first in range(min(limit, n) + 1):
+            # invalid distribution
+            remaining = n - first
+            if remaining > limit * 2:
+                continue
+
+            # 2nd child can get at most min(remaining, limit)
+            upper = min(remaining, limit)
+            # 2nd child can get at least max(0, remaining - limit)
+            lower = max(0, remaining - limit)
+
+            # the range is the number of distributions for 2nd/3rd child
+            # given fixed distribution for 1st child each iteration
+            ans += upper - lower + 1
+
+        return ans
