e48e382c97381544f9e92d2e76599990d03eef15
Sam Wu <22262939+samjwu@users.noreply.github.com>
2025-06-15 18:09:48 -0600

    2016

---
+++ 2016/Solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maximumDifference(self, nums: List[int]) -> int:
+        n = len(nums)
+        max_diff = -1
+
+        for i in range(n):
+            for j in range(i+1, n):
+                diff = nums[j] - nums[i]
+                if diff > max_diff and diff > 0:
+                    max_diff = diff
+
+        return max_diff
