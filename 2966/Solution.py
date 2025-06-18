6dbf05c0ba6348a5c4554d3ac4adeee36d27204d
Sam Wu <22262939+samjwu@users.noreply.github.com>
2025-06-17 18:44:15 -0600

    2966

--- 2966/Solution.py
+++ 2966/Solution.py
@@ -1,15 +1,16 @@
 class Solution:
     def divideArray(self, nums: List[int], k: int) -> List[List[int]]:
-        nums.sort()
-        
         n = len(nums)
-        
-        ans = []
-        
+        arrays = []
+
+        # greedily take smallest triplets
+        nums.sort()
+
         for i in range(0, n, 3):
+            # no valid solution
             if nums[i+2] - nums[i] > k:
                 return []
-            
-            ans.append([nums[i], nums[i+1], nums[i+2]])
-            
-        return ans
+
+            arrays.append([nums[i], nums[i + 1], nums[i + 2]])
+
+        return arrays
