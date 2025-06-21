a32c7a3ed8aea96ec9932a07d9df6d8bf75dd4b6
Sam Wu <22262939+samjwu@users.noreply.github.com>
2025-06-20 18:52:31 -0600

    3085

---
+++ 3085/Solution.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+
+class Solution:
+    def minimumDeletions(self, word: str, k: int) -> int:
+        freq = defaultdict(int)
+
+        for c in word:
+            freq[c] += 1
+
+        removed_char_count = len(word)
+
+        for base_count in freq.values():
+            removed = 0
+
+            for compare_count in freq.values():
+                if base_count > compare_count:
+                    # remove all of compared char
+                    # since cannot raise count of compared char
+                    removed += compare_count
+                elif compare_count > base_count + k:
+                    # reduce compared char
+                    # to match base char plus k
+                    removed += compare_count - (base_count + k)
+
+            removed_char_count = min(removed_char_count, removed)
+
+        return removed_char_count
