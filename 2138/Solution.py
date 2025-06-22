3648aab3f14606bddf15f0137877133f854b4200
Sam Wu <22262939+samjwu@users.noreply.github.com>
2025-06-21 19:01:47 -0600

    2138

---
+++ 2138/Solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def divideString(self, s: str, k: int, fill: str) -> List[str]:
+        substrings = []
+        n = len(s)
+
+        for i in range(0, n, k):
+            substring = s[i:i+k]
+            substrings.append(substring)
+
+        last_length = len(substrings[-1])
+        if last_length != k:
+            diff = k - last_length
+            substrings[-1] = substrings[-1] + fill * diff
+
+        return substrings
