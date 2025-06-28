28ed822da840a04ce597ab3767306efe73528e16
Sam Wu <22262939+samjwu@users.noreply.github.com>
2025-06-27 19:50:23 -0600

    2099

---
+++ 2099/Solution.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+class Solution:
+    def maxSubsequence(self, nums: List[int], k: int) -> List[int]:
+        max_heap = []
+
+        for n in nums:
+            heapq.heappush(max_heap, n)
+            if len(max_heap) > k:
+                heapq.heappop(max_heap)
+
+        freq = Counter(max_heap)
+        max_subseq = []
+
+        for n in nums:
+            if freq[n] > 0:
+                freq[n] -= 1
+                max_subseq.append(n)
+
+        return max_subseq
