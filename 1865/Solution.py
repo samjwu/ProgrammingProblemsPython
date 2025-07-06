1f242b284a895eaecf28f1263261506034d9a6a2
Sam Wu <22262939+samjwu@users.noreply.github.com>
2025-07-05 19:10:13 -0600

    1865

---
+++ 1865/Solution.py
@@ -0,0 +1,29 @@
+from collections import Counter
+from typing import List
+
+class FindSumPairs:
+
+    def __init__(self, nums1: List[int], nums2: List[int]):
+        self.nums1 = nums1
+        self.nums2 = nums2
+        self.freq = Counter(nums2)
+
+    def add(self, index: int, val: int) -> None:
+        self.freq[self.nums2[index]] -= 1
+        self.nums2[index] += val
+        self.freq[self.nums2[index]] += 1
+
+    def count(self, tot: int) -> int:
+        pairs = 0
+        for num in self.nums1:
+            remain = tot - num
+            if remain in self.freq:
+                pairs += self.freq[remain]
+        return pairs
+
+
+
+# Your FindSumPairs object will be instantiated and called as such:
+# obj = FindSumPairs(nums1, nums2)
+# obj.add(index,val)
+# param_2 = obj.count(tot)
