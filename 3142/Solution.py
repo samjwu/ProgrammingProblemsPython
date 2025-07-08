c8ae200006d64c9863ce55075897552eb8b5d244
Sam Wu <22262939+samjwu@users.noreply.github.com>
2025-07-07 19:43:00 -0600

    3142

---
+++ 3142/Solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def satisfiesConditions(self, grid: List[List[int]]) -> bool:
+        m = len(grid)
+        n = len(grid[0])
+
+        for i in range(m):
+            for j in range(n):
+                if (i < m-1 and grid[i][j] != grid[i+1][j]) or (j < n-1 and grid[i][j] == grid[i][j+1]):
+                    return False
+
+        return True
