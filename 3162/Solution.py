bf9500e36769e0e8149828d857b93481ac155510
Sam Wu <22262939+samjwu@users.noreply.github.com>
2025-07-11 20:19:21 -0600

    Precompute nums2 * k

--- 3162/Solution.py
+++ 3162/Solution.py
@@ -3,10 +3,11 @@ from typing import List
 class Solution:
     def numberOfPairs(self, nums1: List[int], nums2: List[int], k: int) -> int:
         pairs = 0
+        nums2k = [num * k for num in nums2]
 
         for i in range(len(nums1)):
             for j in range(len(nums2)):
-                if nums1[i] % (nums2[j] * k) == 0:
+                if nums1[i] % (nums2k[j]) == 0:
                     pairs += 1
 
         return pairs
