f72a0e81770379f0a0c5b583603395ae206a0e78
Sam Wu <22262939+samjwu@users.noreply.github.com>
2025-07-13 18:50:24 -0600

    1290

---
+++ 1290/Solution.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def getDecimalValue(self, head: Optional[ListNode]) -> int:
+        decimal = 0
+
+        while head:
+            decimal *= 2
+            decimal += head.val
+            head = head.next
+
+        return decimal
