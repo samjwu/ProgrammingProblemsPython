b54a0b2490f5c8a5e3a466acb1e7132ddca5aab0
Sam Wu <22262939+samjwu@users.noreply.github.com>
2025-07-18 18:57:14 -0600

    1233 3

---
+++ 1233/Solution3.py
@@ -0,0 +1,24 @@
+from typing import List
+
+class Solution:
+    def removeSubfolders(self, paths: List[str]) -> List[str]:
+        # sort for lexicographical order
+        # to give parent/top folders before subfolders
+        paths.sort()
+
+        n = len(paths)
+
+        # first top folder is given by sorted order
+        top_folders = [paths[0]]
+
+        for i in range(1, n):
+            # create copy to modify
+            prev_parent = top_folders[-1]
+            # add slash to avoid mistakes
+            # example: /a and /ab should not match
+            prev_parent += "/"
+
+            if not paths[i].startswith(prev_parent):
+               top_folders.append(paths[i])
+        
+        return top_folders
