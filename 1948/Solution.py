4f90c8f260f42d8b413fdc9c3337be6a839b5f20
Sam Wu <22262939+samjwu@users.noreply.github.com>
2025-07-19 20:28:16 -0600

    1948

---
+++ 1948/Solution.py
@@ -0,0 +1,88 @@
+from collections import Counter
+from typing import List
+
+class Trie:
+    """
+    Represent all paths as a trie
+
+    each folder is a child node
+    serialized form: parent(child)
+    """
+    children: dict
+    serialized: str
+
+    def __init__(self):
+        self.children = dict()
+        self.serialized = ""
+
+
+class Solution:
+    def deleteDuplicateFolder(self, paths: List[List[str]]) -> List[List[str]]:
+        root = Trie()
+
+        for path in paths:
+            curr = root
+
+            for node in path:
+                if node not in curr.children:
+                    curr.children[node] = Trie()
+                curr = curr.children[node]
+
+        # track count of serialized paths
+        # to later delete duplicated paths
+        freq = Counter()
+
+        def serialize(node: Trie) -> None:
+            """
+            Generate serialized form of each path
+
+            post-order traversal with depth-first search
+            to get children in brackets for serialization
+            for serialized form: parent(child)
+            """
+            # leaf node has no child
+            # therefore no serialized form
+            if not node.children:
+                return
+
+            subfolders = []
+            
+            for subfolder, child in node.children.items():
+                serialize(child)
+                subfolders.append(subfolder + "(" + child.serialized + ")")
+
+            # example input: [["a", "x"], ["a", "z"], ["b", "z"], ["b", "x"]]
+            # if not sorted, would result in duplicates not being deleted since
+            # a.serialized = x()z()
+            # b.serialized = z()x()
+            subfolders.sort()
+            node.serialized = "".join(subfolders)
+            freq[node.serialized] += 1
+
+        serialize(root)
+
+        nonduplicate_folders = []
+        # create temporary variable to record paths
+        temp_path = []
+
+        def exclude_duplicates(node: Trie) -> None:
+            """
+            Find all non duplicate paths
+
+            use counter to avoid duplicated paths
+            """
+            if freq[node.serialized] > 1:
+                return
+
+            if temp_path:
+                nonduplicate_folders.append(temp_path[:])
+
+            for subfolder, child in node.children.items():
+                temp_path.append(subfolder)
+                exclude_duplicates(child)
+                # backtrack to simulate traversing paths
+                temp_path.pop()
+
+        exclude_duplicates(root)
+
+        return nonduplicate_folders
