eb3a2485786b85ecc5ff588d962c1822f940c644
Sam Wu <22262939+samjwu@users.noreply.github.com>
2025-07-20 18:28:26 -0600

    Rename i to read_idx for clarity

--- 1957/solution2.py
+++ 1957/solution2.py
@@ -4,9 +4,9 @@ class Solution:
         chars = list(s)
         write_idx = 2
 
-        for i in range(2, n):
-            if chars[i] != chars[write_idx-1] or chars[i] != chars[write_idx-2]:
-                chars[write_idx] = chars[i]
+        for read_idx in range(2, n):
+            if chars[read_idx] != chars[write_idx-1] or chars[read_idx] != chars[write_idx-2]:
+                chars[write_idx] = chars[read_idx]
                 write_idx += 1
 
         return "".join(chars[0:write_idx])
