da3f36b3a55589ce202fff8f3e0746d1d863f13f
Sam Wu <22262939+samjwu@users.noreply.github.com>
2025-07-21 19:41:36 -0600

    1695

---
+++ 1695/solution.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+
+
+class Solution:
+    def maximumUniqueSubarray(self, nums: List[int]) -> int:
+        window = defaultdict(int)
+        left = 0
+        n = len(nums)
+        window_sum = 0
+        max_score = 0
+
+        for right in range(n):
+            while window[nums[right]] > 0:
+                window[nums[left]] -= 1
+                window_sum -= nums[left]
+                left += 1
+            
+            window[nums[right]] += 1
+            window_sum += nums[right]
+
+            max_score = max(max_score, window_sum)
+
+        return max_score
