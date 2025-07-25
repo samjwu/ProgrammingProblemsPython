55770d57b3e45ff1fbf4368208bfd0c75675a896
Sam Wu <22262939+samjwu@users.noreply.github.com>
2025-07-24 18:18:46 -0600

    3487

---
+++ 3487/solution.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def maxSum(self, nums: List[int]) -> int:
+        high = max(nums)
+
+        if high <= 0:
+            return high
+
+        max_sum = 0
+        seen = set()
+
+        for num in nums:
+            if num > 0 and num not in seen:
+                max_sum += num
+                seen.add(num)
+
+        return max_sum
