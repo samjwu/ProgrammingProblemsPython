1de176d98f91d42710eff1c7c21c4b1166b3c742
Sam Wu <22262939+samjwu@users.noreply.github.com>
2025-07-25 19:12:41 -0600

    Add max index var for clarity

--- 3178/solution.py
+++ 3178/solution.py
@@ -1,13 +1,13 @@
 class Solution:
     def numberOfChild(self, n: int, k: int) -> int:
-        n -= 1
+        max_idx = n - 1
 
-        trips = k // n
-        remaining_steps = k % n
+        trips = k // max_idx
+        remaining_steps = k % max_idx
 
         if trips % 2 == 0:
             # round trip
             return remaining_steps
         else:
             # return trip
-            return n - remaining_steps
+            return max_idx - remaining_steps
