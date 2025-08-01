0d347122b6921d5b0d66597f29a477dfb80e8741
Sam Wu <22262939+samjwu@users.noreply.github.com>
2025-07-31 18:24:37 -0600

    118

---
+++ 118/solution.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        pascals_triangle = [[1]]
+        
+        for i in range(1, numRows):
+            curr_row = [1]
+            
+            for j in range(len(pascals_triangle[i-1])-1):
+                curr_row.append(pascals_triangle[i-1][j] + pascals_triangle[i-1][j+1])
+            
+            curr_row.append(1)
+            pascals_triangle.append(curr_row)
+            
+        return pascals_triangle
