e66d1678f89a297141cda582aed4205395e5bb9e
Sam Wu <22262939+samjwu@users.noreply.github.com>
2025-07-31 18:27:27 -0600

    Account for nonpositive input

--- 118/solution.py
+++ 118/solution.py
@@ -3,6 +3,9 @@ from typing import List
 
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
+        if numRows <= 0:
+            return [[]]
+
         pascals_triangle = [[1]]
         
         for i in range(1, numRows):
