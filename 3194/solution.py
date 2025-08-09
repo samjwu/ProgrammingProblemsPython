3233b71587fbeb1faa80cb20ad95eb3a08205ed3
Sam Wu <22262939+samjwu@users.noreply.github.com>
2025-08-08 20:17:54 -0600

    3194

---
+++ 3194/solution.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def minimumAverage(self, nums: List[int]) -> float:
+        min_avg = float("inf")
+        n = len(nums)
+
+        nums.sort()
+
+        for i in range(n // 2):
+            avg = float(nums[i] + nums[n-1-i]) / 2
+            min_avg = min(min_avg, avg)
+
+        return min_avg
