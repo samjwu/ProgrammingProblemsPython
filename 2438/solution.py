f984fe97b0b846e8498d83a9e871c32b4c3526db
Sam Wu <22262939+samjwu@users.noreply.github.com>
2025-08-10 18:48:50 -0600

    Update comments

--- 2438/solution.py
+++ 2438/solution.py
@@ -7,14 +7,13 @@ class Solution:
 
         # store the powers of two that add up to n
         n_powers = []
-        # 2^0
+        # 2^0 = 1
         curr_power = 1
 
         # find the powers of two that add up to n
         while n > 0:
             if n % 2 == 1:
                 n_powers.append(curr_power)
-                
             n //= 2
             curr_power *= 2
 
@@ -22,7 +21,6 @@ class Solution:
 
         for left, right in queries:
             curr_product = 1
-            # multiply 1 by everything in the bounds of the query
             for i in range(left, right + 1):
                 curr_product = curr_product * n_powers[i] % MOD
             query_products.append(curr_product)
