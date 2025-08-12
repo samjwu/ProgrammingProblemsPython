2827d81408ae81b143c135825e8687531b2ac8fa
Sam Wu <22262939+samjwu@users.noreply.github.com>
2025-08-11 18:34:17 -0600

    2787

---
+++ 2787/solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numberOfWays(self, n: int, x: int) -> int:
+        MOD = 10 ** 9 + 7
+
+        @cache
+        def recurse(n: int, x: int, unique_int: int) -> int:
+            remaining = n - unique_int ** x
+
+            if remaining == 0:
+                # found a way
+                return 1
+            elif remaining > 0:
+                # don't include current unique int
+                exclude_unique = recurse(n, x, unique_int + 1)
+                # include current unique int
+                include_unique = recurse(remaining, x, unique_int + 1)
+                # total ways is all ways both including and excluding unique int
+                return exclude_unique + include_unique
+            else:
+                # unique int ^ x is too high, no valid solution
+                return 0
+
+        return recurse(n, x, 1) % MOD
