40f13ab65dbcb5a4e5754b36bcff5e9531ee09a1
Sam Wu <22262939+samjwu@users.noreply.github.com>
2025-08-16 08:29:14 -0600

    1323

---
+++ 1323/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        digits = [digit for digit in str(num)]
+        n = len(digits)
+
+        for i in range(n):
+            if digits[i] == '6':
+                digits[i] = '9'
+                return int("".join(digits))
+
+        return num
