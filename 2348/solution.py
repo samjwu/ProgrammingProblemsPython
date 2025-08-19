e21b9ea3c0ff1134d2091ed38c8cb0ef35b7ef13
Sam Wu <22262939+samjwu@users.noreply.github.com>
2025-08-18 19:08:05 -0600

    2348

---
+++ 2348/solution.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def zeroFilledSubarray(self, nums: List[int]) -> int:
+        total = 0
+        current = 0
+
+        for num in nums:
+            if num == 0:
+                current += 1
+                total += current
+            else:
+                current = 0
+
+        return total
