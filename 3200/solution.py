1d5244bbecf25095ad7be92b5ae45c16d9108f5b
Sam Wu <22262939+samjwu@users.noreply.github.com>
2025-08-24 18:22:27 -0600

    3200

---
+++ 3200/solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxHeightOfTriangle(self, red: int, blue: int) -> int:
+        def simulate_height(type_1: int, type_2: int) -> int:
+            height = 0
+            row_number = 1
+
+            while True:
+                if row_number % 2 == 1:
+                    if type_1 >= row_number:
+                        type_1 -= row_number
+                    else:
+                        break
+                else:
+                    if type_2 >= row_number:
+                        type_2 -= row_number
+                    else:
+                        break
+
+                row_number += 1
+                height += 1
+
+            return height
+
+        return max(simulate_height(red, blue), simulate_height(blue, red))
